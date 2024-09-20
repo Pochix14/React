b9b963bb9db7262072c6dce28c5efb00d0c02e3c
Pablo Rodriguez Navarro <44807688+Pochix14@users.noreply.github.com>
2024-09-20 14:28:41 -0600

    State update
    Add function to update cart state, without duplicate items, only quantity

--- guitarla/src/components/Guitar.jsx
+++ guitarla/src/components/Guitar.jsx
@@ -1,9 +1,13 @@
 
 
-export default function Guitar ({guitar}) {
+export default function Guitar ({guitar, addToCart}) {
 
-    const handleClick = (id) => {
-        console.log("ID: ", id)
+    //La funcion se puede poner directamente en el onclick del boton
+    //Se puede pasar el carrito de compras, pero en automatico al utilizar un arrow function,
+    //se sabe que un parametro que se pasa el es estado previo del state, o sea, el carrito
+    const handleClick = (guitar) => {
+        //setCart([...cart, guitar])
+        setCart(prevCart => [...prevCart, guitar]) //Sin la necesidad de pasar el carrito
     }
 
 
@@ -19,7 +23,7 @@ export default function Guitar ({guitar}) {
                     <button 
                         type="button"
                         className="btn btn-dark w-100"
-                        onClick={() => handleClick(guitar.id)}
+                        onClick={() => addToCart(guitar)/*handleClick(guitar)*/}
                     >Agregar al Carrito</button>
                 </div>
             </div>
